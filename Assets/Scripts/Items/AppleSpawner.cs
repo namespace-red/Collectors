a08b47e6825b9cd640929c75c0d146d2c1266b35
namespace-red <67328203+namespace-red@users.noreply.github.com>
2025-03-21 15:53:23 +0300

    AppleSpawner

---
+++ Assets/Scripts/Items/AppleSpawner.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using UnityEngine;
+
+public class AppleSpawner : Spawner<Apple>
+{
+    [SerializeField] private float _secCoolDown = .5f;
+
+    protected override void OnValidate()
+    {
+        base.OnValidate();
+        
+        if (_secCoolDown < 0)
+            _secCoolDown = 0;
+    }
+    
+    private void OnEnable()
+    {
+        StartCoroutine(StartCreating());
+    }
+
+    private IEnumerator StartCreating()
+    {
+        var wait = new WaitForSeconds(_secCoolDown);
+
+        while (enabled)
+        {
+            yield return wait;
+            Get();
+        }
+    }
+}
